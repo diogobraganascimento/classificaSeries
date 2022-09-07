d8b408ab79debd08ed27ce5a82dff03aec56b3a5
Diogo Nascimento <diogonascimentobraga@gmail.com>
2022-09-07 19:49:57 -0300

    Method criar
    variáveis saindo da função index e tornando global;
    
    função 'criar' inserida no projeto;

--- classificaSeries.py
+++ classificaSeries.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 class Serie:
@@ -8,21 +8,33 @@ class Serie:
         self.plataforma = plataforma
 
 
+serie1 = Serie('Luis Miguel', 'BiogrÃ¡fica', 'netflix')
+serie2 = Serie('The Witcher', 'Fantasia', 'Netflix')
+serie3 = Serie('Hanna', 'aÃ§Ã£o', 'Prime Video')
+lista = [serie1, serie2, serie3]
+
+
 app = Flask(__name__)
 
 
 @app.route('/inicio')
 def index():
-    serie1 = Serie('Luis Miguel', 'BiogrÃ¡fica', 'netflix')
-    serie2 = Serie('The Witcher', 'Fantasia', 'Netflix')
-    serie3 = Serie('Hanna', 'aÃ§Ã£o', 'Prime Video')
-    lista = [serie1, serie2, serie3]
     return render_template('lista.html', titulo='series', series=lista)
 
 
 @app.route('/novo')
 def novo():
     return render_template('novo.html', titulo='nova seria')
+
+
+@app.route('/criar')
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    plataforma = request.form['plataaforma']
+    serie = Serie(nome, categoria, plataforma)
+    lista.append(serie)
+    return render_template('lista.html', titulo='Series', series=lista)
 
 
 app.run()
