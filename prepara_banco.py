158bca885baeb33820bfeb9383870f73d64d281b
Diogo Nascimento <diogonascimentobraga@gmail.com>
2022-09-13 14:58:29 -0300

    Criação do BD
    Efetuado a criação do arquivos preprara_banco.py e dao.py para conexão, criação e inserção de dados no banco de dados MySQL.

--- prepara_banco.py
+++ prepara_banco.py
@@ -1,20 +1,14 @@
-import psycopg2
+import MySQLdb
 print('Conectando...')
-conn = psycopg2.connect(
-      host="localhost",
-      database="classificaSerie",
-      port="5432",
-      user='admin',
-      password='admin'
-      )
+conn = MySQLdb.connect(user='root', passwd='root', host='127.0.0.1', port=3306)
 
 # Descomente se quiser desfazer o banco...
-conn.cursor().execute("DROP DATABASE `classificaSerie`;")
-conn.commit()
+# conn.cursor().execute("DROP DATABASE `classificaSeries`;")
+# conn.commit()
 
 criar_tabelas = '''SET NAMES utf8;
-    CREATE DATABASE `classificaSerie` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
-    USE `classificaSerie`;
+    CREATE DATABASE `classificaSeries` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
+    USE `classificaSeries`;
     CREATE TABLE `serie` (
       `id` int(11) NOT NULL AUTO_INCREMENT,
       `nome` varchar(50) COLLATE utf8_bin NOT NULL,
@@ -34,31 +28,31 @@ conn.cursor().execute(criar_tabelas)
 # inserindo usuarios
 cursor = conn.cursor()
 cursor.executemany(
-      'INSERT INTO classificaSerie.usuario (id, nome, senha) VALUES (%s, %s, %s)',
+      'INSERT INTO classificaSeries.usuario (id, nome, senha) VALUES (%s, %s, %s)',
       [
             ('luan', 'Luan Marques', 'flask'),
             ('nico', 'Nico', '7a1'),
             ('danilo', 'Danilo', 'vegas')
       ])
 
-cursor.execute('select * from classificaSerie.usuario')
+cursor.execute('select * from classificaSeries.usuario')
 print(' -------------  Usuários:  -------------')
 for user in cursor.fetchall():
     print(user[1])
 
 # inserindo series
 cursor.executemany(
-      'INSERT INTO classificaSerie.serie (nome, categoria, plataforma) VALUES (%s, %s, %s)',
+      'INSERT INTO classificaSeries.serie (nome, categoria, plataforma) VALUES (%s, %s, %s)',
       [
-            ('A casa do Dração', 'Fantasia', 'HBO'),
-            ('Luis Miguel', 'Biografia', 'netflix'),
-            ('The Witcher', 'Fantasia', 'Netflix'),
-            ('Hanna', 'acao', 'Prime Video'),
-            ('House, M.D.', 'Médica', 'Prime Video'),
-            ('Greys Anatomy', 'Médica', 'Prime Video'),
+          ('A casa do Dração', 'Fantasia', 'HBO'),
+          ('Luis Miguel', 'Biografia', 'netflix'),
+          ('The Witcher', 'Fantasia', 'Netflix'),
+          ('Hanna', 'acao', 'Prime Video'),
+          ('House, M.D.', 'Médica', 'Prime Video'),
+          ('Greys Anatomy', 'Médica', 'Prime Video'),
       ])
 
-cursor.execute('select * from classificaSerie.serie')
+cursor.execute('select * from classificaSeries.serie')
 print(' -------------  series:  -------------')
 for serie in cursor.fetchall():
     print(serie[1])
